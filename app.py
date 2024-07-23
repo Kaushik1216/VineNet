f1d400478ae50c01ae83c7c7736e868558ffb9c4
Kaushik1216 <kaushik.vishwakarma2003@gmail.com>
2024-07-24 03:29:22 +0530

    Make necessary change for host website and remove storage requirement

--- app.py
+++ app.py
@@ -1,29 +1,20 @@
-
- 
-from enum import Enum
-from io import BytesIO, StringIO
-from typing import Union
- 
-import pandas as pd
+from io import BytesIO
 import streamlit as st
-import os
 from ultralytics import YOLO
 import torch
-from torchvision import transforms
 from PIL import Image
 import io
-import time
 import cv2
 import numpy as np
-from pathlib import Path
 from streamlit_option_menu import option_menu
-from streamlit_webrtc import webrtc_streamer , RTCConfiguration, VideoTransformerBase
-import av
+from streamlit_webrtc import webrtc_streamer
+import threading
+
 EXAMPLE_NO = 1
 
 with open('style.css') as f:
     st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
- 
+
 def streamlit_menu(example=2):
         selected = option_menu(
             menu_title=None,
@@ -35,224 +26,167 @@ def streamlit_menu(example=2):
         )
         return selected
 
-
 selected = streamlit_menu(example=EXAMPLE_NO)
+
+def load_model():
+    model_path = "best.pt"
+    model = YOLO(model_path)
+    return model
 
 if selected == "image":
     st.title(f"You have selected {selected}")
-    
-    
-    class FileUpload(object):
-        def __init__(self , model):
-            self.fileTypes = ["csv", "png", "jpg"]
-            self.model = model
+    file = st.file_uploader("Upload file", type=["jpeg", "png", "jpg"])
+    show_file = st.empty()
+    FRAME_WINDOW = st.image([])
+
+    class ImageClass(object):
+        def __init__(self):
+            self.model = load_model()
+            self.image = np.zeros(6, np.uint8)
     
         def run(self):
-            """
-            Upload File on Streamlit Code
-            :return:
-            """
 
-            file = st.file_uploader("Upload file", type=self.fileTypes)
-            show_file = st.empty()
             if not file:
                 image_path = './image.png'
                 image = Image.open(image_path)
-                image = np.array(image)
-                process(image , model)
-                st.image('out.png', caption="Example Segmentated Image")
-                show_file.info("Please upload a file of type: " + ", ".join(["csv", "png", "jpg"]))
+                self.image = np.array(image)
+                self.process()
                 return
 
             if isinstance(file, BytesIO):
                 st.image(file, caption="Uploaded Image")
                 image_bytes = file.read()
-                # Convert bytes to PIL Image
                 image = Image.open(io.BytesIO(image_bytes))
-                image = np.array(image)
-                process(image , self.model)
-                st.image('out.png', caption="Predicted Image")
-            file.close()
+                self.image = np.array(image)
+                self.process()
 
+        def process(self):
 
-    def load_model():
-            model_path = "best.pt"
-            model = YOLO(model_path)
+                image = self.image
+                image = image[:, :, ::-1]
 
-            return model
-    def process(image , model):
-
-        # Reorder channels (assuming RGB order in PIL image)
-            image = image[:, :, ::-1]
-
-            # Perform inference
-            with torch.no_grad():
-                output = model.predict(image)
-                output = output[0]
-                output.save("./out.png")
-
-            image_path = './out.png'
-
-            image = Image.open(image_path)
-
-            # st.image(image, caption='vinenetimage')
-
-            with open(image_path, "rb") as file:
-                btn = st.download_button(
-                        label="Download Seg image",
-                        type = 'primary',
-                        data=file,
-                        file_name="image_k.png",
-                        mime="image/png"
-                    )
+                with torch.no_grad():
+                    output = self.model.predict(image)
+                    output = cv2.cvtColor(output[0].plot(), cv2.COLOR_BGR2RGB)
+                    FRAME_WINDOW.image(output, use_column_width=True, caption="Segmentated Image")
+                    is_success, buffer = cv2.imencode(".png", output)
+                    if is_success:
+                        st.download_button(
+                            label="Download Segmented image",
+                            data=buffer.tobytes(),
+                            file_name="predicted_image.png",
+                            mime="image/png"
+                        )
 
     if __name__ ==  "__main__":
-        model = load_model()
-        helper = FileUpload(model)
-        helper.run()
+        imageobject = ImageClass()
+        imageobject.run()
 
 if selected == "video":
     st.title(f"You have selected {selected}")
+    FRAME_WINDOW = st.image([])
 
-    FRAME_WINDOW = st.image([]) #frame window
+    class VideoClass(object):
+        def __init__(self):
+            self.model = load_model()
 
+        def run(self):
 
-    st.markdown("""<h2 style='text-align: center;'>Please Check start to start video.</h2>""", unsafe_allow_html=True) #title
+            lock = threading.Lock()
+            img_container = {"img": None}
 
-    # run = st.checkbox("Start") #checkbox
-    #capture video
-    model_path = "best.pt"
-    model = YOLO(model_path)# weights are stored and accessed using the path given 
-    
-    # if run == True: # frame will render 
-    #     capture_vid=cv2.VideoCapture(0) # capturing the video 
-    #     capture_vid.set(3,640)
-    #     capture_vid.set(4,480)
-    #     while True:
-    #         ret,frame = capture_vid.read()
-    #         # frame= cv2.cvtColor(frame,cv2.COLOR_BGR2RGB) #convert bgr to rgb format 
-    #         results=model.predict(frame) # fitting the model 
-    #         result = results[0]
+            def video_frame_callback(frame):
+                img = frame.to_ndarray(format="bgr24")
+                with lock:
+                    img_container["img"] = img
 
-    #         result.save("./out.png")
-    #         image_path = './out.png'
+                return frame
 
-    #         image = Image.open(image_path) 
-    #         FRAME_WINDOW.image(image) # adding the image in frame window after complete processing
-    #         time.sleep(1)
+            ctx = webrtc_streamer(key="example", video_frame_callback=video_frame_callback, media_stream_constraints={"video": True, "audio": False})
 
-    def callback(frame):
-        # img = frame.to_ndarray(format="bgr24")
+            while ctx.state.playing:
+                with lock:
+                    img = img_container["img"]
+                if img is None:
+                    continue
 
-        # img = cv2.Canny(img , 100, 200)
-        # img = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
-        # return av.VideoFrame.from_ndarray(img , format="bgr24")
-        model_path = "best.pt"
-        model = YOLO(model_path)
-        results=model.predict(frame) # fitting the model 
-        result = results[0]
+                with torch.no_grad():
+                    output = self.model.predict(img)
+                    output = output[0].plot()
+                FRAME_WINDOW.image(cv2.cvtColor(output, cv2.COLOR_BGR2RGB), use_column_width=True)
 
-        result.save("./out.png")
-        image_path = './out.png'
-
-        image = Image.open(image_path) 
-
-        return image
-
-    
-    webrtc_streamer(key="sample" , video_frame_callback=callable,media_stream_constraints={"video": True, "audio": False})
+    if __name__ ==  "__main__":
+        videoobject = VideoClass()
+        videoobject.run()
 
 
 if selected == "mask":
     st.title(f"You have selected {selected}")
+    file = st.file_uploader("Upload file", type=["jpeg", "png", "jpg"])
+    show_file = st.empty()
+    FRAME_WINDOW = st.image([])
 
-    class FileUpload(object):
-        def __init__(self , model):
-            self.fileTypes = ["csv", "png", "jpg"]
-            self.model = model
+    class MaskClass(object):
+        def __init__(self):
+            self.model = load_model()
+            self.image = np.zeros(6, np.uint8)
     
         def run(self):
-            """
-            Upload File on Streamlit Code
-            :return:
-            """
 
-            file = st.file_uploader("Upload file", type=self.fileTypes)
-            show_file = st.empty()
             if not file:
                 image_path = './image.png'
                 image = Image.open(image_path)
-                image = np.array(image)
-                process(image , model)
-                st.image('out.png', caption="Example Segmentated Image")
-                show_file.info("Please upload a file of type: " + ", ".join(["csv", "png", "jpg"]))
+                self.image = np.array(image)
+                self.process()
                 return
 
             if isinstance(file, BytesIO):
                 st.image(file, caption="Uploaded Image")
-                process(file , self.model)
-                st.image('out.png', caption="Predicted Image")
-            file.close()
+                image_bytes = file.read()
+                image = Image.open(io.BytesIO(image_bytes))
+                self.image = np.array(image)
+                self.process()
 
-    def load_model():
-            model_path = "best.pt"
-            model = YOLO(model_path)
+        def process(self):
 
-            return model
-    def process(image , model):
+                image = self.image
+                image = image[:, :, ::-1]
 
-        # Reorder channels (assuming RGB order in PIL image)
-            image = image[:, :, ::-1]
+                with torch.no_grad():
+                    output = self.model.predict(image)
+                    for r in output:
+                        img = np.copy(r.orig_img)
 
-            # Perform inference
-            with torch.no_grad():
-                output = model.predict(image)
-                for r in output:
-                    img = np.copy(r.orig_img)
-                    img_name = Path(r.path).stem # source image base-name
+                        b_mask = np.zeros(img.shape[:2], np.uint8)
 
-                    # Create binary mask
-                    b_mask = np.zeros(img.shape[:2], np.uint8)
+                        for ci,c in enumerate(r):
+                            #  Get detection class name
+                            label = c.names[c.boxes.cls.tolist().pop()]
+                            #  Extract contour result
+                            contour = c.masks.xy.pop()
+                            #  Changing the type
+                            contour = contour.astype(np.int32)
+                            #  Reshaping
+                            contour = contour.reshape(-1, 1, 2)
 
-                    # Iterate each object contour (multiple detections)
-                    for ci,c in enumerate(r):
-                        #  Get detection class name
-                        label = c.names[c.boxes.cls.tolist().pop()]
-                        #  Extract contour result
-                        contour = c.masks.xy.pop()
-                        #  Changing the type
-                        contour = contour.astype(np.int32)
-                        #  Reshaping
-                        contour = contour.reshape(-1, 1, 2)
-
-
-                        # Draw contour onto mask
-                        try:
-                            predict_mask = cv2.drawContours(b_mask,
-                                                [contour],
-                                                -1,
-                                                (255, 255, 255),
-                                                cv2.FILLED)
-                        except:
-                            continue
-                cv2.imwrite('./out.png', b_mask)
-
-            image_path = './out.png'
-
-            image = Image.open(image_path)
-
-            # st.image(image, caption='vinenetimage')
-
-            with open(image_path, "rb") as file:
-                btn = st.download_button(
-                        label="Download image",
-                        type = 'primary',
-                        data=file,
-                        file_name="image_k.png",
-                        mime="image/png"
+                            # Draw contour onto mask
+                            try:
+                                predict_mask = cv2.drawContours(b_mask,
+                                                    [contour],
+                                                    -1,
+                                                    (255, 255, 255),
+                                                    cv2.FILLED)
+                            except:
+                                continue
+                    FRAME_WINDOW.image(b_mask, use_column_width=True, caption="Segmentated Mask")
+                    is_success, buffer = cv2.imencode(".png", b_mask)
+                    st.download_button(
+                        label="Download Segmented Mask",
+                        data=buffer.tobytes(),
+                        file_name="predicted_image.png",
+                        mime="image_mask/png"
                     )
 
     if __name__ ==  "__main__":
-        model = load_model()
-        helper = FileUpload(model)
+        helper = MaskClass()
         helper.run()
